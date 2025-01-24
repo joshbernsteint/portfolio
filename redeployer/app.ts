6418beda5a10e7e19c9bf497b27c7504f2817aad
Joshua Bernstein <joshuatbernstein@gmail.com>
2025-01-23 23:01:30 -0500

    modified redeployer

--- redeployer/app.ts
+++ redeployer/app.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import { config } from 'dotenv';
 import { spawnSync } from 'child_process';
 
+function log(str: string){
+    console.log(`${new Date().toISOString()}: ${str}`);
+}
+
 config();
 const PORT = process.env.PORT || 3000;
 const dest_dir = process.env.DEST;
@@ -34,6 +38,7 @@ let activeTimeout : NodeJS.Timeout | undefined = undefined;
 const DELAY = 5000;
 
 app.get('/', async (req,res) => {
+    log("Server pinged");
     res.send("Ping route!");
 });
 
@@ -47,10 +52,10 @@ app.post('/github', async (req,res) => {
         
         const branchName = body.ref.split('/')[2];
         if(branchName === "main"){
-            console.log(`${new Date().toString()}: Reloading Server`);
+            log("Reloading Server...")
             runServer();
             res.send("Server reloaded!");
-            console.log("Server reloaded");
+            log("Server reloaded");
             
             activeTimeout = setTimeout(() => {activeTimeout = undefined}, DELAY);
         }
@@ -59,6 +64,7 @@ app.post('/github', async (req,res) => {
 });
 
 app.listen(PORT, () => {
+    log("Starting, rebuilding static files...")
     runServer();
-    console.log("Server Online!");
+    log("Server Online!");
 });
